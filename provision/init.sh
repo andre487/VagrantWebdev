b8e87d3579f37d7e336e50d7e5088ec3a0019b04
Andre <andrey.prokopyuk@gmail.com>
2014-01-22 14:36:47 +0400

    настройка PostgreSQL

--- provision/init.sh
+++ provision/init.sh
@@ -18,6 +18,7 @@ apt-get install -y bind9 dnsutils apache2 php5 mysql-server mysql-client php5 ph
 service bind9 stop
 service apache2 stop
 service mysql stop
+service postgresql stop
 
 #Apache
 a2enmod rewrite
@@ -37,6 +38,11 @@ kill $PID
 sleep 10s
 echo "Go on"
 
+#PostgreSQL
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.1/main/postgresql.conf
+echo "postgres:password" | /usr/sbin/chpasswd
+cp /vagrant/provision/data/postgres/pg_hba.conf /etc/postgresql/9.1/main/
+
 #DNS
 cp /vagrant/provision/data/bind9/db.loc /etc/bind
 cp /vagrant/provision/data/bind9/named.conf.local /etc/bind
@@ -50,3 +56,4 @@ chattr +i /etc/resolv.conf
 service bind9 start
 service apache2 start
 service mysql start
+service postgresql start
