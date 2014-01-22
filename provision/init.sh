cc43fe000ef6c63696f7385e6a42681da07d766f
Andre <andrey.prokopyuk@gmail.com>
2014-01-22 15:30:52 +0400

    настройка Memcached

--- provision/init.sh
+++ provision/init.sh
@@ -4,12 +4,12 @@ set -e
 
 
 #
-#Apt operations
+#Install goods
 #
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get upgrade -y --no-install-recommends
-apt-get install -y bind9 dnsutils apache2 php5 mysql-server mysql-client php5 php-pear php5-mysql sqlite postgresql python-pip python-mysqldb python-imaging vim
+apt-get install -y bind9 dnsutils apache2 mysql-server mysql-client memcached php5 php-pear php5-mysql php5-memcache sqlite postgresql redis-server python-pip python-mysqldb python-imaging python-redis python-memcache git vim
 
 
 #
@@ -19,6 +19,8 @@ service bind9 stop
 service apache2 stop
 service mysql stop
 service postgresql stop
+service redis-server stop
+service memcached stop
 
 #Apache
 a2enmod rewrite
@@ -43,6 +45,12 @@ sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgre
 echo "postgres:password" | /usr/sbin/chpasswd
 cp /vagrant/provision/data/postgres/pg_hba.conf /etc/postgresql/9.1/main/
 
+#Redis
+sed -i 's/bind 127.0.0.1/#bind 127.0.0.1/g' /etc/redis/redis.conf
+
+#Memcached
+sed -i 's/-l 127.0.0.1/#-l 127.0.0.1/g' /etc/memcached.conf
+
 #DNS
 cp /vagrant/provision/data/bind9/db.loc /etc/bind
 cp /vagrant/provision/data/bind9/named.conf.local /etc/bind
@@ -57,3 +65,5 @@ service bind9 start
 service apache2 start
 service mysql start
 service postgresql start
+service redis-server start
+service memcached start
