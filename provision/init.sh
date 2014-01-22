221be703581e4a18ace038f10dd5adb3a330fe8d
Andre <andrey.prokopyuk@gmail.com>
2014-01-22 12:43:42 +0400

    настройка bind9 для зоны .loc

--- provision/init.sh
+++ provision/init.sh
@@ -2,21 +2,27 @@
 
 set -e
 
+#
 #Apt operations
+#
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get upgrade -y --no-install-recommends
-apt-get install -y dnsmasq nginx apache2 php5 mysql-server mysql-client php5 php-pear php5-mysql sqlite postgresql python-pip python-mysqldb python-imaging
+apt-get install -y bind9 dnsutils apache2 libapache2-mod-macro php5 mysql-server mysql-client php5 php-pear php5-mysql sqlite postgresql python-pip python-mysqldb python-imaging vim
 
+#
 #Configure
-service dnsmasq stop
-service nginx stop
+#
+service bind9 stop
 service apache2 stop
 service mysql stop
 
+#Apache
 a2enmod rewrite
+a2enmod macro
 a2ensite default
 
+#MySQL
 mysqld_safe --init-file=/vagrant/provision/data/mysql-init.sql &> /dev/null &
 echo "Wait for MySQL..."
 sleep 10s
@@ -26,8 +32,13 @@ kill $PID
 sleep 10s
 echo "Go on"
 
+#DNS
+cp /vagrant/provision/data/bind9/db.loc /etc/bind
+cp /vagrant/provision/data/bind9/named.conf.local /etc/bind
+cp /vagrant/provision/data/resolv.conf /etc
+chattr +i /etc/resolv.conf
+
 #Reboot
-service dnsmasq start
-service nginx start
+service bind9 start
 service apache2 start
 service mysql start
