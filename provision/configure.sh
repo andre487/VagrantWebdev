9f2ad61585bafa44d72a546d92480f63bbffa7b5
Andre <andrey.prokopyuk@gmail.com>
2014-02-08 17:03:59 +0400

    включен вывод ошибок в PHP

--- provision/configure.sh
+++ provision/configure.sh
@@ -107,6 +107,10 @@ if [ ! -d /vagrant/runtime/xdebug ]; then
 fi
 cp /vagrant/provision/data/php/xdebug.ini /etc/php5/conf.d/99-xdebug.ini
 
+sed -i "s/display_errors = Off/display_errors = On/g" /etc/php5/apache2/php.ini
+sed -i "s/display_startup_errors = Off/display_startup_errors = On/g" /etc/php5/apache2/php.ini
+sed -i "s/error_reporting = E_ALL & ~E_DEPRECATED & ~E_STRICT/error_reporting = E_ALL/g" /etc/php5/apache2/php.ini
+
 # MySQL
 sed -i "s/bind-address/#bind-address/g" /etc/mysql/my.cnf
 
